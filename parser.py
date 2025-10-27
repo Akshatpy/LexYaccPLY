7d76f26df64e8d2a9621040f2d644386b851d5ef
Akshat Sharma <akshatomega@gmail.com>
2025-10-27 21:18:22 +0530

    Added projsummary pdf

--- parser.py
+++ parser.py
@@ -1,10 +1,7 @@
 # parser.py
-
 import ply.yacc as yacc
 from lexer import tokens
-
 start = 'statement'
-
 def p_statement(p):
     '''statement : variable_declaration
                  | if_else_statement
@@ -14,11 +11,9 @@ def p_statement(p):
                  | dictionary
     '''
     p[0] = p[1]
-
 def p_variable_declaration(p):
     'variable_declaration : ID EQUALS value'
     p[0] = ('var_assign', p[1], p[3])
-
 def p_value(p):
     '''value : expression
              | STRING
@@ -28,7 +23,6 @@ def p_value(p):
              | ID
     '''
     p[0] = p[1]
-
 def p_if_else_statement(p):
     '''if_else_statement : IF condition COLON statement ELSE COLON statement
                          | IF condition COLON statement
@@ -37,7 +31,6 @@ def p_if_else_statement(p):
         p[0] = ('if-else', p[2], p[4], p[7])
     else:
         p[0] = ('if', p[2], p[4])
-
 def p_condition(p):
     '''condition : expression GT expression
                  | expression LT expression
@@ -48,11 +41,9 @@ def p_condition(p):
         p[0] = (p[2], p[1], p[3])
     else:
         p[0] = p[1]
-
 def p_function_definition(p):
     'function_definition : DEF ID LPAREN arg_list RPAREN COLON statement'
     p[0] = ('func_def', p[2], p[4], p[7])
-
 def p_arg_list(p):
     '''arg_list : ID
                 | ID COMMA arg_list
@@ -64,7 +55,6 @@ def p_arg_list(p):
         p[0] = [p[1]] + p[3]
     else:
         p[0] = []
-
 def p_list(p):
     'list : LBRACKET item_list RBRACKET'
     p[0] = ('list', p[2])
@@ -80,7 +70,6 @@ def p_item_list(p):
         p[0] = [p[1]] + p[3]
     else:
         p[0] = []
-
 def p_dictionary(p):
     'dictionary : LBRACE pair_list RBRACE'
     p[0] = ('dict', p[2])
@@ -96,13 +85,11 @@ def p_pair_list(p):
         p[0] = [p[1]] + p[3]
     else:
         p[0] = []
-
 def p_pair(p):
     '''pair : STRING COLON value
             | NUMBER COLON value
     '''
     p[0] = (p[1], p[3])
-
 def p_expression(p):
     '''expression : term PLUS term
                   | term MINUS term
@@ -112,7 +99,6 @@ def p_expression(p):
         p[0] = (p[2], p[1], p[3])
     else:
         p[0] = p[1]
-
 def p_term(p):
     '''term : factor TIMES factor
             | factor DIVIDE factor
@@ -122,7 +108,6 @@ def p_term(p):
         p[0] = (p[2], p[1], p[3])
     else:
         p[0] = p[1]
-
 def p_factor(p):
     '''factor : NUMBER
               | ID
@@ -131,8 +116,7 @@ def p_factor(p):
     if len(p) == 4:
         p[0] = p[2]
     else:
-        p[0] = p[1]
-        
+        p[0] = p[1] 
 def p_empty(p):
     'empty :'
     pass
@@ -142,5 +126,4 @@ def p_error(p):
     else:
         message = "Syntax error at end of input"
     raise SyntaxError(message)
-
 parser = yacc.yacc()
